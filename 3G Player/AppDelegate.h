698fed179498685aed8dd0b59761849b8663e7de
themylogin <themylogin@gmail.com>
2013-07-02 13:18:54 +0300

    Initial Commit

---
+++ 3G Player/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  3G Player
+//
+//  Created by Admin on 7/2/13.
+//  Copyright (c) 2013 themylogin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
